bff15e03e2fe70be3b0905cec171dee491df8105
EseAlli <essealli.ea@gmail.com>
2022-11-21 11:50:48 +0100

    add env file to gitignore

--- server/index.js
+++ server/index.js
@@ -2,4 +2,4 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.listen(port, console.log("running on port" + port));
+app.listen(port, console.log("running on port " + port));
