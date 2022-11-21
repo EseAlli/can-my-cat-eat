541270dce2fbc339c4e9ce6f1df62e4829476eda
EseAlli <essealli.ea@gmail.com>
2022-11-21 12:10:29 +0100

    configuration for mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.listen(port, console.log("running on port " + port));
